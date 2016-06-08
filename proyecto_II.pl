fefc241a22021fde4120de12ab8edce45597162d
franciscosucre <frank91frank@gmail.com>
2016-06-08 17:54:50 -0430

    (FJSG) Ya funciona con los ejemplos de la guia el bienEtiquetado

--- proyecto_II.pl
+++ proyecto_II.pl
@@ -1,23 +1,63 @@
-etiquetas(nodo(etiqueta,_, Retorno)) :- Retorno = etiqueta.
-etiquetas(arista(etiqueta,_, Retorno)) :- Retorno = etiqueta.
+% etiquetas(+nodo,-Retorno)
+% Obtiene la etiqueta de un nodo
+% 	+nodo: Nodo del cual se extraera la etiqueta
+%	-Retorno: Etiqueta extraida
+etiquetas(nodo(Etiqueta,_),Retorno) :- Retorno is Etiqueta.
 
-obtenerNodoDestino(arista(_,nodo,Retorno)) :- Retorno = nodo.
+% etiquetas(+arista,-Retorno)
+% Obtiene la etiqueta de un nodo
+% 	+arista: Arista de la cual se extraera la etiqueta
+%	-Retorno: Etiqueta extraida
+etiquetas(arista(Etiqueta,_),Retorno) :- Retorno is Etiqueta.
+
+% obtenerNodoDestino(+arista,-Retorno)
+% Obtiene el nodo al que lleva la arista
+% 	+arista: Arista a examenar
+%	-Retorno: Nodo extraido
+obtenerNodoDestino(arista(_,Nodo),Retorno):- Retorno = Nodo.
 
 % bienEtiquetado(+Arbol)
+% Verifica si un arbol esta bien etiquetado o no.
+% 	Arbol: Arbol a examenar
+
+% Caso Base: Nodo Unitario
 bienEtiquetado(nodo(_,[])).
-bienEtiquetado(nodo(et,[H|T])) :-
+% Caso 1: Arbol con mas de un nodo
+bienEtiquetado(nodo(Et,[H|T])) :-
 	% Obtenemos el nodo al que lleva la arista
-	obtenerNodoDestino(H,nodoDestino),
+	obtenerNodoDestino(H,NodoDestino),
+	% Obtenemos la etiqueta de la arista seleccionada
+	etiquetas(H,EtiquetaArista),
 	% Obtenemos su etiqueta
-	etiquetas(nodoDestino,etiquetaDest),
+	etiquetas(NodoDestino,EtiquetaDest),
 	% Calculamos la diferencia entre la etiqueta del
 	% nodo origen a la etiqueta del nodo destino
-	resta is abs(etiquetaDest - et),
+	Resta is abs(EtiquetaDest - Et),
 	% Verificamos que se cumple la formula de buen etiquetado
 	% e=|a-b|
-	et \== resta,
+	EtiquetaArista =:= Resta,
 	% Analizamos el arbol que esta al final de
 	% la arista seleccionada
-	bienEtiquetado(nodoDestino),
+	bienEtiquetado(NodoDestino),
 	% Analizamos la siguiente arista
-	bienEtiquetado(nodo(et,T)).
+	bienEtiquetado(nodo(Et,T)).	
+
+% Estructura de Datos
+% esq([[3],[0,0,0]])
+
+% esqueleto(+N,+R,-esqueleto)
+% 	N : Numero de Niveles Del Arbol
+%	R : Numero de hijos Por N
+%	esqueleto: Arbol Resultante
+
+% Debe satisfacerse o re-satisfacerse con todos los esqueletos de árboles R-arios con N>0 nodos en el esqueleto
+
+% etiquetamiento(+Esqueleto,-Arbol)
+
+% Debe satisfacerse si Arbol es un buen etiquetamiento de Esqueleto
+
+% esqEtiquetables(+R,+N)
+
+% Debe satisfacerse si todos los esqueletos de árboles R-arios con N nodos son bien etiquetables 
+
+% describirEtiquetamiento(+Arbol)
--- pruebas.pl
+++ pruebas.pl
@@ -5,4 +5,7 @@
 % bienEtiquetado(nodo(3,[])).
 
 % Arbol Con 2 nodos y una arista
-% bienEtiquetado(nodo(3,[arista(1,[nodo(2,[])])])).
+% bienEtiquetado(nodo(3,[arista(1,nodo(2,[]))])).
+
+% Ejemplo Complejo del enunciado
+% bienEtiquetado(nodo(4,[arista(1,nodo(3,[])),arista(2,nodo(2,[])),arista(3,nodo(1,[]))])).
