4d38972cb7ae36e67fe933c1eef766399c4011a6
franciscosucre <frank91frank@gmail.com>
2016-06-07 23:06:06 -0430

    (FJSG) Iniciando proyecto, bienetiquetado se tranca en el caso base y no avanza

--- proyecto_II.pl
+++ proyecto_II.pl
@@ -1,5 +1,23 @@
 etiquetas(nodo(etiqueta,_, Retorno)) :- Retorno = etiqueta.
 etiquetas(arista(etiqueta,_, Retorno)) :- Retorno = etiqueta.
 
+obtenerNodoDestino(arista(_,nodo,Retorno)) :- Retorno = nodo.
+
 % bienEtiquetado(+Arbol)
 bienEtiquetado(nodo(_,[])).
+bienEtiquetado(nodo(et,[H|T])) :-
+	% Obtenemos el nodo al que lleva la arista
+	obtenerNodoDestino(H,nodoDestino),
+	% Obtenemos su etiqueta
+	etiquetas(nodoDestino,etiquetaDest),
+	% Calculamos la diferencia entre la etiqueta del
+	% nodo origen a la etiqueta del nodo destino
+	resta is abs(etiquetaDest - et),
+	% Verificamos que se cumple la formula de buen etiquetado
+	% e=|a-b|
+	et \== resta,
+	% Analizamos el arbol que esta al final de
+	% la arista seleccionada
+	bienEtiquetado(nodoDestino),
+	% Analizamos la siguiente arista
+	bienEtiquetado(nodo(et,T)).
---
+++ pruebas.pl
@@ -0,0 +1,8 @@
+% Archivo que contiene casos de prueba, estos deben
+% copiarse en la linea de comando de prolog
+
+% Arbol Con 1 nodo
+% bienEtiquetado(nodo(3,[])).
+
+% Arbol Con 2 nodos y una arista
+% bienEtiquetado(nodo(3,[arista(1,[nodo(2,[])])])).
