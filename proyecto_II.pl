267bc9d831ce7734f0fa3476c363de7de6312346
franciscosucre <frank91frank@gmail.com>
2016-06-08 22:12:53 -0430

    (FJSG) Se implemento una funcion que construye niveles para el esqueleto

--- proyecto_II.pl
+++ proyecto_II.pl
@@ -45,10 +45,79 @@ bienEtiquetado(nodo(Et,[H|T])) :-
 % Estructura de Datos
 % esq([[3],[0,0,0]])
 
+% obtenerLista(+Esqueleto,-Lista)
+% Obtiene la lista de un esqueleto
+% 	Esqueleto: Esqueleto del cual extraer la lista
+%	Lista: Lista Extraida
+
+% Caso 1: No hay esqueleto previo
+obtenerLista(esq([H|T]),ListaRetorno) :-
+	ListaRetorno = [H|T].
+% Caso 2: No hay esqueleto previo
+obtenerLista(_,[]).
+
+% obtenerCabeza(lista,-Elemento).
+obtenerCabeza([H|T],Elemento) :-
+	Elemento = H.
+
+% construirNivel(R0,Restante, ListaEntrada,ListaRetorno).
+% Construye un nivel del esqueleto con el maximo de elementos
+%	R0: Maximo numero de nodos por nivel
+%	Restante: Nodos restantes por agregar
+%	ListaEntrada: Lista de entrada para la recursion
+%	ListaRetorno: Lista de salida
+
+% Caso Base: No quedan nodos por agregar
+construirNivel(_,0, ListaEntrada,ListaRetorno) :-
+	% Retornamos el resultado
+	ListaRetorno = ListaEntrada.
+
+% Caso 1: Estamos agregando el primer Nodo
+construirNivel(R0,R0, _,ListaRetorno) :-
+	RestanteNuevo is R0 - 1,
+	construirNivel(R0, RestanteNuevo, [R0|[]], ListaRetorno2),
+	ListaRetorno = ListaRetorno2.
+
+% Caso 2: Estamos agregando otros nodos ademas del primero
+construirNivel(R0,Restante, ListaEntrada,ListaRetorno) :-
+	ListaTemp = [R0|ListaEntrada],
+	RestanteNuevo is Restante - 1,
+	construirNivel(R0,RestanteNuevo, ListaTemp ,ListaRetorno2),
+	ListaRetorno = ListaRetorno2.
+	
+
 % esqueleto(+N,+R,-esqueleto)
-% 	N : Numero de Niveles Del Arbol
-%	R : Numero de hijos Por N
-%	esqueleto: Arbol Resultante
+% 	+N : Numero de Niveles Del Arbol
+%	+R : Numero de hijos Por N
+%	-esqueleto: Arbol Resultante
+
+% Caso Base: Arbol con 1 Nodo
+esqueleto(1,_,Esqueleto).
+
+% Caso 2: Arbol con mas de 1 nivel, pero con el contador de hijos en 0
+esqueleto(N,0,Esqueleto) :-
+	N > 1,
+	N is N - 1.
+
+% Caso 1: Arbol con mas de 1 Nodo
+esqueleto(N,R,Esqueleto):-
+	% Verificamos que N sea mayor a 0
+	N > 0,
+	% Verificamos que R sea mayor a 0
+	R > 0,
+	% Obtenemos la lista
+	obtenerLista(Esqueleto,ListaEsqueleto),
+	% Construimos el nivel necesario
+	construirNivel(R, R, [], NivelTemp),
+	
+	
+	% Agregamos un nodo al nivel
+	
+	
+	R1 is R - 1,
+	N1 is N - 1,
+	% Devolvemos el esqueleto final
+	Esqueleto = esq(ListaEsqueleto).
 
 % Debe satisfacerse o re-satisfacerse con todos los esqueletos de árboles R-arios con N>0 nodos en el esqueleto
 
